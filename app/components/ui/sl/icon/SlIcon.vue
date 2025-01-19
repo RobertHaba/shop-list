d47dedcd6744d1caf9c407f847f8358127e25439
RobertHaba <sedkipl98@gmail.com>
2025-01-19 19:07:16 +0100

    feat: add SlIcon component for reusable icon rendering with customizable size

---
+++ app/components/ui/sl/icon/SlIcon.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+interface Props {
+  name: string
+  size?: number | string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <Icon class="shrink-0" :name :size />
+</template>
