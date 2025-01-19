195f2d03371270d109152379a72b1aefecd8438b
RobertHaba <sedkipl98@gmail.com>
2025-01-19 14:19:20 +0100

    feat: redirect logged-in users to the app dashboard instead of home

--- app/middleware/unauth.ts
+++ app/middleware/unauth.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
 
   if (loggedIn.value) {
-    return navigateTo('/')
+    return navigateTo('/app/')
   }
 })
