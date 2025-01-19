e2a23d920f7bb3098ce10eff7ff86f5923566486
RobertHaba <sedkipl98@gmail.com>
2025-01-19 19:28:30 +0100

    feat: add AppDashboardHeader component for user greeting and avatar display

---
+++ app/components/app/dashboard/AppDashboardHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const userStore = useUserStore()
+</script>
+
+<template>
+  <div class="h-10 flex items-center justify-between">
+    <div class="text-sm flex gap-1 text-muted-foreground">
+      <span>{{ $t('app.dashboard.welcome') }}</span>
+
+      <span class="truncate max-w-36">{{ userStore.user?.name }}</span>  👋
+    </div>
+
+    <SlAvatar :name="userStore.user?.name!" />
+  </div>
+</template>
