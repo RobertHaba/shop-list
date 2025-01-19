9407181073dd9d48693de3e617c4829a24c73250
RobertHaba <sedkipl98@gmail.com>
2025-01-19 19:07:09 +0100

    feat: add mobile navigation bar and item components for improved app navigation

---
+++ app/components/app/AppMobileNavbar.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const { t } = useI18n()
+
+const menuItemList = [
+  { to: '/app', icon: 'tabler:home', title: t('app.menu.home') },
+  { to: '/app/list/add', icon: 'tabler:square-rounded-plus', title: t('app.menu.newList') },
+  { to: '/app/list/favorites', icon: 'tabler:heart', title: t('app.menu.favorites') },
+]
+</script>
+
+<template>
+  <div class="w-full h-20 bg-slate-200 flex items-center justify-evenly px-4 dark:bg-slate-800 sticky bottom-0 rounded-t-3xl">
+    <AppMobileNavbarItem v-for="item in menuItemList" :key="item.title" v-bind="item" />
+  </div>
+</template>
---
+++ app/components/app/AppMobileNavbarItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+interface Props {
+  to: string
+  icon: string
+  title: string
+}
+
+const { to, icon } = defineProps<Props>()
+
+const route = useRoute()
+
+const isActiveRoute = computed(() => route.path === to)
+
+const resolvedIcon = computed(() => isActiveRoute.value ? `${icon}-filled` : icon)
+</script>
+
+<template>
+  <NuxtLink class="flex flex-col items-center w-fit gap-0.5 text-foreground [&.router-link-active]:text-primary [&.router-link-active]:bg-primary/10 px-4 py-1 rounded-lg" :to>
+    <SlIcon class="size-6" :name="resolvedIcon" />
+
+    <span class="text-xs font-semibold">{{ title }}</span>
+  </NuxtLink>
+</template>
