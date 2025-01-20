62c2ea33a9359f3389000f42c131285851f66dfa
RobertHaba <sedkipl98@gmail.com>
2025-01-20 20:54:36 +0100

    feat: add AppMobile layout component for mobile view structure

---
+++ app/layouts/appMobile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative flex flex-col h-screen overflow-hidden">
+    <slot name="header" />
+
+    <div class="h-full flex flex-col">
+      <slot />
+    </div>
+
+    <AppMobileNavbar />
+  </div>
+</template>
