5b0aaadf74172cbc6c69a3734ad2bf622f67312d
RobertHaba <sedkipl98@gmail.com>
2025-01-20 20:52:45 +0100

    feat: add SlButton component for routing with customizable button behavior

---
+++ app/components/ui/sl/button/SlButton.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import type { RouteLocationRaw } from 'vue-router'
+import type { CnButtonProps } from '~/components/ui/cn/button/Button.vue'
+
+interface Props extends CnButtonProps {
+  to: RouteLocationRaw
+}
+
+const { to } = defineProps<Props>()
+
+const asComponent = computed(() => to ? resolveComponent('NuxtLink') : 'button')
+</script>
+
+<template>
+  <CnButton v-bind="$props" :to :as="asComponent">
+    <slot />
+  </CnButton>
+</template>
