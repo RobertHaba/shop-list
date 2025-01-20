19b4e077dfd23fb1248d5e81008c01bc645636cf
RobertHaba <sedkipl98@gmail.com>
2025-01-20 20:55:02 +0100

    feat: add product detail page with dynamic title and navigation

---
+++ app/pages/app/products/[id]/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+const route = useRoute()
+const { t } = useI18n()
+
+const isNewList = computed(() => route.params.id === 'new')
+
+const title = computed(() => isNewList.value ? t('app.productList.newList') : 'BCK')
+</script>
+
+<template>
+  <NuxtLayout name="app-container">
+    <AppMobileHeader :title />
+
+    <AppProductList class="h-full" />
+
+    <SlButton class="w-full" size="lg" :to="`/app/products/${$route.params.id}/add`">
+      {{ $t('global.add') }}
+    </SlButton>
+  </NuxtLayout>
+</template>
