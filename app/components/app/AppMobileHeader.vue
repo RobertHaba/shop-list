b2607f061b42016fa2a814b3d946b37ebcb66409
RobertHaba <sedkipl98@gmail.com>
2025-01-20 20:53:40 +0100

    feat: add AppMobileHeader component for mobile header layout and navigation

---
+++ app/components/app/AppMobileHeader.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+interface Props {
+  title?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <header class="flex gap-2 h-10 items-center">
+    <CnButton size="icon" variant="ghost" @click="$router.back()">
+      <SlIcon class="size-8 text-muted-foreground" name="tabler:arrow-narrow-left" />
+    </CnButton>
+
+    <h1 class="text-lg font-semibold">
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<style>
+
+</style>
