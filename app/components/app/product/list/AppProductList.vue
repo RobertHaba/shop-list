feb50f4fb6c70e6f3424e4fb09c43d432b331b3e
RobertHaba <sedkipl98@gmail.com>
2025-01-20 20:53:57 +0100

    feat: add AppProductList and AppProductListItem components for product listing and interaction

---
+++ app/components/app/product/list/AppProductList.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const list = [
+  { icon: 'tabler:a-b', name: 'Pomidor' },
+  { icon: 'tabler:a-b', name: 'Cebula' },
+]
+</script>
+
+<template>
+  <SlCard>
+    <div>
+      <AppProductListItem v-for="item in list" :key="item.name" v-bind="item" />
+    </div>
+  </SlCard>
+</template>
---
+++ app/components/app/product/list/AppProductListItem.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+interface Props {
+  name: string
+  icon: string
+}
+
+defineProps<Props>()
+
+const itemRef = useTemplateRef('itemRef')
+
+const { isSwiping, direction } = useSwipe(itemRef)
+</script>
+
+<template>
+  <div ref="itemRef" class="border-b flex justify-between border-border items-center min-h-14" :class="{ 'bg-muted': isSwiping }">
+    <div class="flex gap-4 items-center">
+      <CnCheckbox class="size-6 rounded-full" />
+
+      <div class="flex gap-2 items-center">
+        <SlIcon :name="icon" />
+
+        <h3 class="text-sm">
+          {{ name }}
+        </h3>
+      </div>
+    </div>
+
+    <CnBadge class="bg-muted text-muted-foreground">
+      2
+    </CnBadge>
+  </div>
+</template>
