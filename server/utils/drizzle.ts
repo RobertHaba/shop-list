db317403388608726b0bc10ed25c1bff2e6591f5
RobertHaba <sedkipl98@gmail.com>
2025-01-23 20:43:57 +0100

    feat: add snake_case casing option to drizzle connection and infer types for ShoppingLists and ShoppingListItems

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -5,7 +5,11 @@ import * as schema from '~~/server/database/schema'
 export const tables = schema
 
 export function useDrizzle() {
-  return drizzle({ connection: useRuntimeConfig().databaseUrl, schema })
+  return drizzle({ connection: useRuntimeConfig().databaseUrl, schema, casing: 'snake_case' })
 }
 
 export type User = typeof schema.users.$inferSelect
+
+export type ShoppingLists = typeof schema.shoppingLists.$inferSelect
+
+export type ShoppingListItems = typeof schema.shoppingListItems.$inferSelect
