cb1be09211f74be218e78d6a66bbbe045fd53442
RobertHaba <sedkipl98@gmail.com>
2025-01-24 21:31:30 +0100

    feat: add SlTextHint component

---
+++ app/components/ui/sl/text/SlTextHint.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <p class="text-xs text-muted-foreground">
+    <slot>{{ text }}</slot>
+  </p>
+</template>
