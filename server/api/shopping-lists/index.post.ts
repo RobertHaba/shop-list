3169f5ef4ccb182782ae41c69f3fbdee73d1ec9d
RobertHaba <sedkipl98@gmail.com>
2025-01-25 17:08:18 +0100

    feat: add API endpoint for inserting shopping lists

---
+++ server/api/shopping-lists/index.post.ts
@@ -0,0 +1,22 @@
+import { shoppingListInsertSchema } from '~~/server/database/schema'
+
+export default defineEventHandler(async (event) => {
+  const { user } = await requireUserSession(event)
+
+  const body = await readBody(event)
+
+  validator.validateSchema(shoppingListInsertSchema, body)
+
+  const inserted = await useDrizzle().insert(tables.shoppingLists).values({
+    userId: user.id,
+    name: body.name,
+  }).returning({
+    id: tables.shoppingLists.id,
+    name: tables.shoppingLists.name,
+  })
+
+  return {
+    success: true,
+    data: inserted[0],
+  }
+})
