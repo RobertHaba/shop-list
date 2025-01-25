db22e86e2b956a47429ed9ff0f9e38509a5bb96e
RobertHaba <sedkipl98@gmail.com>
2025-01-25 17:03:26 +0100

    feat: enhance AppShoppingListItem component with product statistics and improved template

--- app/components/app/shopping/list/AppShoppingListItem.vue
+++ app/components/app/shopping/list/AppShoppingListItem.vue
@@ -1,13 +1,43 @@
-<template>
-  <div>
-    
-  </div>
-</template>
-
 <script lang="ts" setup>
+interface Props {
+  name: string
+  id: number
+  createdAt: string | null
+  userId: number
+  productCount: number
+  completedProductCount: number
+}
 
+const props = defineProps<Props>()
+
+const { completedPercent, totalToBuy } = useStats()
+
+function useStats() {
+  const completedPercent = computed(() => props.productCount > 1 ? (props.completedProductCount / props.productCount) * 100 : 0)
+
+  const totalToBuy = computed(() => props.productCount - props.completedProductCount)
+
+  return {
+    completedPercent,
+    totalToBuy,
+  }
+}
 </script>
 
-<style>
+<template>
+  <NuxtLink :to="`/app/products/${id}`" class="flex flex-col justify-between gap-2 p-4 bg-card rounded-lg">
+    <h3 class="font-semibold">
+      {{ name }}
+    </h3>
 
-</style>
+    <div class="flex flex-col gap-2">
+      <div class="flex gap-2 items-center">
+        <CnProgress :model-value="completedPercent" />
+
+        <span class="text-xs">{{ completedProductCount }}/{{ productCount }}</span>
+      </div>
+
+      <SlTextHint v-if="totalToBuy" :text="$t('app.shoppingList.completeListAndBuyProducts', { nProducts: $t('global.nProducts', totalToBuy) })" />
+    </div>
+  </NuxtLink>
+</template>
