4d05822f56057b2311418f490d1c5f1fd452dd96
RobertHaba <sedkipl98@gmail.com>
2025-01-25 17:28:16 +0100

    feat: enhance shopping list component to display items dynamically

--- app/components/app/shopping/list/AppShoppingList.vue
+++ app/components/app/shopping/list/AppShoppingList.vue
@@ -1,13 +1,13 @@
-<template>
-  <div>
-    
-  </div>
-</template>
-
 <script lang="ts" setup>
+import AppShoppingListItem from './AppShoppingListItem.vue'
 
+const { data } = useFetch('/api/shopping-lists')
 </script>
 
-<style>
-
-</style>
+<template>
+  <div class="flex flex-col gap-4">
+    <template v-if="data?.data?.length">
+      <AppShoppingListItem v-for="item in data.data" :key="item.id" v-bind="item" />
+    </template>
+  </div>
+</template>
