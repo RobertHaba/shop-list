0eb09756db8906f7655e7e2b53561d5fdd927a49
RobertHaba <sedkipl98@gmail.com>
2025-01-26 21:24:35 +0100

    feat: add API endpoint for updating items in a shopping list

---
+++ server/api/shopping-lists/[listId]/items/[itemId].put.ts
@@ -0,0 +1,36 @@
+import { and, eq } from 'drizzle-orm'
+import { shoppingListItems, shoppingListItemsUpdateSchema } from '~~/server/database/schema'
+
+export default defineEventHandler(async (event) => {
+  const { user } = await requireUserSession(event)
+
+  const { listId, itemId } = getRouterParams(event)
+
+  const numericListId = Number(listId)
+  const numericItemId = Number(itemId)
+
+  const body = await readBody(event)
+
+  validator.validateSchema(shoppingListItemsUpdateSchema, body)
+
+  const [updated] = await useDrizzle()
+    .update(shoppingListItems)
+    .set(body)
+    .where(
+      and(
+        eq(shoppingListItems.id, numericItemId),
+        eq(shoppingListItems.listId, numericListId),
+        eq(shoppingListItems.userId, user.id),
+      ),
+    )
+    .returning()
+
+  if (!updated) {
+    throw createError({
+      status: 404,
+      message: 'error.server.itemNotFound',
+    })
+  }
+
+  return updated
+})
