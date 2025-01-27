33a3c0733b99cdaf02d09954831666c8bae7a86c
RobertHaba <sedkipl98@gmail.com>
2025-01-27 21:58:20 +0100

    feat: add new product addition page with mobile header and search input

---
+++ app/pages/app/products/[id]/add.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+const route = useRoute()
+const { t } = useI18n()
+
+const isNewList = computed(() => route.params.id === 'new')
+</script>
+
+<template>
+  <NuxtLayout name="app-container">
+    <AppMobileHeader :back-to="`/app/products/${$route.params.id}`">
+      <SlInput name="search" icon-prepend="tabler:search" />
+    </AppMobileHeader>
+  </NuxtLayout>
+</template>
