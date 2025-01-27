d5b145722ed8acdaee8c5f83c41e2ee8437efd78
RobertHaba <sedkipl98@gmail.com>
2025-01-27 22:01:47 +0100

    feat: add shopping list detail page with mobile header and product list

---
+++ layers/shoppingList/pages/app/shopping-list/[id].vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+
+const route = useRoute()
+const { t } = useI18n()
+
+const shoppingListStore = useShoppingListStore()
+
+shoppingListStore.getList(String(route.params.id))
+
+const { title } = useListData()
+
+function useListData() {
+  const isNewList = computed(() => route.params.id === 'new')
+
+  const title = computed(() => isNewList.value ? t('app.productList.newList') : shoppingListStore.list?.name)
+
+  return { title }
+}
+</script>
+
+<template>
+  <NuxtLayout name="app-container">
+    <AppMobileHeader :title back-to="/app" />
+
+    <AppProductList class="h-full" />
+
+    <SlButton class="w-full" size="lg" :to="`/app/products/${$route.params.id}/add`">
+      {{ $t('global.add') }}
+    </SlButton>
+  </NuxtLayout>
+</template>
