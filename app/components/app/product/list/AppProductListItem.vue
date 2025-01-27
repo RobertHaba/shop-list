f7f7a90c5b1bd8e21b7cd9795abd4cd18afec793
RobertHaba <sedkipl98@gmail.com>
2025-01-27 18:18:46 +0100

    feat: update AppProductListItem to use item prop and toggle completion status

--- app/components/app/product/list/AppProductListItem.vue
+++ app/components/app/product/list/AppProductListItem.vue
@@ -1,26 +1,50 @@
 <script lang="ts" setup>
+type Item = ApiResponse<'/api/shopping-lists/:listId', 'get'>['products'][0]
+
 interface Props {
-  name: string
-  icon: string
+  item: Item
 }
 
-defineProps<Props>()
+const { item } = defineProps<Props>()
+
+const route = useRoute()
 
 const itemRef = useTemplateRef('itemRef')
 
 const { isSwiping, direction } = useSwipe(itemRef)
+
+const { toggleChecked } = useItemActions()
+
+const resolvedIcon = computed(() => 'tabler:cactus')
+
+function useItemActions() {
+  async function toggleChecked() {
+    const res = await $fetch(`/api/shopping-lists/${route.params.id}/items/${item.id}`, {
+      method: 'PUT',
+      body: {
+        isCompleted: !item.isCompleted,
+      },
+    })
+
+    console.log(res)
+  }
+
+  return {
+    toggleChecked,
+  }
+}
 </script>
 
 <template>
   <div ref="itemRef" class="border-b flex justify-between border-border items-center min-h-14" :class="{ 'bg-muted': isSwiping }">
     <div class="flex gap-4 items-center">
-      <CnCheckbox class="size-6 rounded-full" />
+      <CnCheckbox :checked="item.isCompleted" class="size-6 rounded-full" @update:checked="toggleChecked" />
 
       <div class="flex gap-2 items-center">
-        <SlIcon :name="icon" />
+        <SlIcon :name="resolvedIcon" />
 
         <h3 class="text-sm">
-          {{ name }}
+          {{ item.name }}
         </h3>
       </div>
     </div>
