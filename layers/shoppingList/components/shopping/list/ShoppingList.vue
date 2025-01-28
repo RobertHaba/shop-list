40daeb634652f49d5bc53f66cbde4dac75023e61
RobertHaba <sedkipl98@gmail.com>
2025-01-28 16:46:26 +0100

    feat: enhance shopping list display with empty state and footer button

--- layers/shoppingList/components/shopping/list/ShoppingList.vue
+++ layers/shoppingList/components/shopping/list/ShoppingList.vue
@@ -3,9 +3,21 @@ const { data } = useFetch('/api/shopping-lists')
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 overflow-auto">
+  <SlCard :label="$t('shopping.list.title')">
     <template v-if="data?.data?.length">
       <ShoppingListItem v-for="item in data.data" :key="item.id" :item />
     </template>
-  </div>
+
+    <div v-else class="flex flex-col gap-3">
+      <p class="text-sm">
+        {{ $t('shopping.list.empty') }}
+      </p>
+    </div>
+
+    <template #footer>
+      <SlButton class="w-full sm:w-fit" to="/app/shopping-list/new">
+        {{ $t('shopping.createList') }}
+      </SlButton>
+    </template>
+  </SlCard>
 </template>
