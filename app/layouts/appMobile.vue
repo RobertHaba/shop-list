06dc39ae9ce2764799221897db0db9c877455142
RobertHaba <sedkipl98@gmail.com>
2025-01-28 16:15:24 +0100

    fix: add overflow-auto to appMobile layout for better scrolling behavior

--- app/layouts/appMobile.vue
+++ app/layouts/appMobile.vue
@@ -2,7 +2,7 @@
   <div class="relative flex flex-col h-screen overflow-hidden">
     <slot name="header" />
 
-    <div class="h-full flex flex-col">
+    <div class="h-full flex flex-col overflow-auto">
       <slot />
     </div>
 
