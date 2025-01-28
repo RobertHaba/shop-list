a7c28eb3952979ccc5d50b233338c948b4b5e77a
RobertHaba <sedkipl98@gmail.com>
2025-01-28 16:15:18 +0100

    feat: dynamically set layout based on route path in app component

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 // init user store
 useUserStore()
+
+const route = useRoute()
+
+const layoutName = computed(() => route.path.startsWith('/app') ? 'app-mobile' : 'default')
 </script>
 
 <template>
   <div class="bg-background text-foreground min-h-screen flex flex-col">
-    <NuxtLayout name="app-mobile">
+    <NuxtLayout :name="layoutName">
       <NuxtPage />
     </NuxtLayout>
 
