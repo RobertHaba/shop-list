17f3850e99eadea7b95dcc0927e30528a08ed034
RobertHaba <sedkipl98@gmail.com>
2025-01-31 23:06:59 +0100

    fix: allow custom class for SlIcon component

--- app/components/ui/sl/icon/SlIcon.vue
+++ app/components/ui/sl/icon/SlIcon.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
 interface Props {
   name: string
   size?: number | string
+  class?: HTMLAttributes['class']
 }
 
 defineProps<Props>()
 </script>
 
 <template>
-  <Icon class="shrink-0" :name :size />
+  <Icon class="shrink-0" :name :size :class="$props.class" />
 </template>
