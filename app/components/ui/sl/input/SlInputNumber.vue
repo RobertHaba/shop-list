268af16fbc78ed51da8c860ca8d17a114e0191ec
RobertHaba <sedkipl98@gmail.com>
2025-01-31 20:24:04 +0100

    feat: add SlInputNumber component for numeric input handling in forms

---
+++ app/components/ui/sl/input/SlInputNumber.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+interface SlInputProps {
+  type?: string
+  name: string
+  textKey?: string
+  label?: string
+  placeholder?: string
+  hideLabel?: boolean
+  required?: boolean
+  description?: string
+  iconPrepend?: string
+  classInput?: HTMLAttributes['class']
+}
+
+const props = defineProps<SlInputProps>()
+
+const { generateFieldTextAttrs } = useFormFieldAttrs()
+
+const { resolvedLabel, resolvedPlaceholder } = generateFieldTextAttrs(props)
+</script>
+
+<template>
+  <CnFormField v-slot="{ componentField, handleChange, value }" :name>
+    <CnFormItem>
+      <CnNumberField :min="0" :model-value="value" @update:model-value="handleChange">
+        <CnFormLabel v-if="resolvedLabel">
+          {{ resolvedLabel }}
+        </CnFormLabel>
+
+        <CnNumberFieldContent>
+          <CnNumberFieldDecrement>
+            <slot name="decrement" />
+          </CnNumberFieldDecrement>
+
+          <CnFormControl>
+            <CnNumberFieldInput :class="$props.classInput" :placeholder="resolvedPlaceholder" v-bind="componentField" />
+          </CnFormControl>
+
+          <CnNumberFieldIncrement />
+        </CnNumberFieldContent>
+
+        <CnFormMessage />
+      </CnNumberField>
+    </CnFormItem>
+  </CnFormField>
+</template>
