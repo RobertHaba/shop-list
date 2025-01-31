da4d5ca74c949ece0b33db31711fee7d16b9a297
RobertHaba <sedkipl98@gmail.com>
2025-01-31 20:23:57 +0100

    feat: add ShoppingAddProduct component for adding products to shopping list

---
+++ layers/shoppingList/components/shopping/addProduct/ShoppingAddProduct.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const productListStore = useProductListStore()
+
+productListStore.getProductList()
+</script>
+
+<template>
+  <CnDrawer>
+    <CnDrawerTrigger>Open</CnDrawerTrigger>
+
+    <CnDrawerContent>
+      <div class="mx-auto w-full flex flex-col gap-2 py-6 max-w-lg">
+        <CnDrawerHeader class="gap-4">
+          <CnDrawerTitle class="text-white">
+            {{ $t('shopping.product.addProductToList') }}
+          </CnDrawerTitle>
+
+          <SlInput name="search" icon-prepend="tabler:search" type="search" />
+        </CnDrawerHeader>
+
+        <div class="px-4">
+          <ul class="flex flex-col gap-2">
+            <li v-for="product in productListStore.productList" :key="product.id">
+              <ShoppingAddProductListItem :product />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </CnDrawerContent>
+  </CnDrawer>
+</template>
