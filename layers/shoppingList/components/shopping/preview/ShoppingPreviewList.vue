ea056c5482cef6bff82399a496463d9a1c93a92d
RobertHaba <sedkipl98@gmail.com>
2025-01-31 21:18:25 +0100

    fix: enhance ShoppingPreviewList layout with flexbox and add footer for product addition

--- layers/shoppingList/components/shopping/preview/ShoppingPreviewList.vue
+++ layers/shoppingList/components/shopping/preview/ShoppingPreviewList.vue
@@ -3,7 +3,13 @@ const shoppingListStore = useShoppingListStore()
 </script>
 
 <template>
-  <SlCard v-if="shoppingListStore.list">
-    <ShoppingPreviewListItem v-for="item in shoppingListStore.list.items" :key="item.id" :item />
+  <SlCard v-if="shoppingListStore.list" class="h-full">
+    <div class="flex-1">
+      <ShoppingPreviewListItem v-for="item in shoppingListStore.list.items" :key="item.id" :item />
+    </div>
+
+    <template #footer>
+      <ShoppingAddProduct />
+    </template>
   </SlCard>
 </template>
