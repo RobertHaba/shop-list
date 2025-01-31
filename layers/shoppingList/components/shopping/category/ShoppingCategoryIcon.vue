283b9425dc9746bd21fb3a512c98993d6f4c3884
RobertHaba <sedkipl98@gmail.com>
2025-01-31 23:37:56 +0100

    feat: add ShoppingCategoryIcon component for dynamic category icons

---
+++ layers/shoppingList/components/shopping/category/ShoppingCategoryIcon.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { CategoryIcon } from '~~/layers/shoppingList/constants/'
+
+interface Props {
+  icon: string | null
+}
+
+const { icon } = defineProps<Props>()
+
+const iconName = computed(() => (CategoryIcon[icon as keyof typeof CategoryIcon] ?? CategoryIcon.fruits))
+</script>
+
+<template>
+  <div class="p-1 flex place-content-center bg-background rounded-full">
+    <SlIcon :name="iconName" />
+  </div>
+</template>
